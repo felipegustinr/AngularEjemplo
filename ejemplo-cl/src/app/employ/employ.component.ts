1f9f8e1fef32f9a1a4532bb6882fc89bba7505d0
felipeGustin <dfgustin.4560@unicesmag.edu.co>
2024-04-10 17:43:43 -0500

    Create employ component

---
+++ ejemplo-cl/src/app/employ/employ.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employ',
+  standalone: true,
+  imports: [],
+  templateUrl: './employ.component.html',
+  styleUrl: './employ.component.css'
+})
+export class EmployComponent {
+
+}
